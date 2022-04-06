4b8f3fd95e255002b5681693ec8aeb7abcf0325b
BCS21090011 <BCS21090011@student.ucts.edu.my>
2022-04-06 20:38:26 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class JustATesting
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Can you see me?");
+
+        Console.ReadLine();
+    }
+}
